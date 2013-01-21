816cba633b78c0e6a11e560d8447a4aac071be6c
Héctor <hector0193@gmail.com>
2013-01-21 21:32:29 +0100

    Demo map loads :D

--- src/jgame/JGame.java
+++ src/jgame/JGame.java
@@ -5,6 +5,7 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
 
 
 /**
@@ -12,7 +13,9 @@ import org.newdawn.slick.SlickException;
  * @author hector
  */
 public class JGame extends BasicGame
-{   
+{
+    private TiledMap map;
+    
     public JGame()
     {
         super("JGame");
@@ -23,7 +26,7 @@ public class JGame extends BasicGame
         try
         {
             AppGameContainer app = new AppGameContainer(new JGame());
-            app.setDisplayMode(500, 400, false);
+            app.setDisplayMode(800, 600, false);
             app.start();
         }
         catch (SlickException e)
@@ -35,14 +38,18 @@ public class JGame extends BasicGame
     @Override
     public void init(GameContainer container) throws SlickException
     {
+        map = new TiledMap("res/levels/demo.tmx");
     }
 
     @Override
     public void update(GameContainer container, int delta) throws SlickException
     {
+        
     }
-
+    
+    @Override
     public void render(GameContainer container, Graphics g) throws SlickException
     {
+        map.render(0, 0);
     }
 }
